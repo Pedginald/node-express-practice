ef555cabcc68f4be81998f0e59e89eeca580e715
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-07-21 12:57:47 -0400

    Creating server with html responses

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	// console.log(req.method);
+	const url = req.url;
+	// Homepage
+	if (url === '/') {
+		res.writeHead(200, { 'content-type': 'text/html' })
+		res.write('<h1>Homepage</h1>')
+		res.end()
+		// About page
+	} else if (url === '/about') {
+		res.writeHead(200, { 'content-typ': 'text/htm' })
+		res.write('<h1>About</h1>')
+		res.end()
+		// Invalid page url
+	} else {
+		res.writeHead(404, { 'content-type': 'text/html' })
+		res.write('<h1>Page not found</h1>')
+		res.end()
+	}
+})
+
+server.listen(5000);
