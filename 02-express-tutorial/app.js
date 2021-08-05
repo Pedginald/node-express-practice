1694425576a747b1da9a590f68ef3611948b4501
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-05 18:35:19 -0400

    Creating middleware example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,53 +1,20 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
 
-app.get('/', (req, res) => {
-	return res.send('<h1>Home Page</h1><a href="/api/products">Products<a>')
+const logger = (req, res, next) => {
+	const method = req.method;
+	const url = req.url;
+	const time = new Date().getFullYear();
+	console.log(method, url, time);
+	next();
+}
+
+app.get('/', logger, (req, res) => {
+	return res.send('Home');
 })
 
-app.get('/api/products', (req, res) => {
-	const newProducts = products.map(product => {
-		const { id, name, image } = product;
-		return { id, name, image };
-	})
-	res.json(newProducts);
-})
-
-app.get('/api/products/:productID', (req, res) => {
-	console.log(req.params);
-	const { productID } = req.params;
-	const singleProduct = products.find(product => {
-		return product.id === Number(productID);
-	})
-	if (!singleProduct) {
-		return res.status(404).send('This product does not exist!');
-	}
-	return res.json(singleProduct);
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-	console.log(req.params);
-	return res.send('Hello World!');
-})
-
-app.get('/api/v1/query', (req, res) => {
-	let sortedProducts = [...products]
-	const { search, limit } = req.query;
-	// console.log(req.query);
-
-	if (search) {
-		sortedProducts = sortedProducts.filter(product => {
-			return product.name.startsWith(search);
-		})
-	}
-	if (limit) {
-		sortedProducts = sortedProducts.slice(0, Number(limit));
-	}
-	if (sortedProducts.length < 1) {
-		return res.status(200).json({ success: true, data: [] })
-	}
-	res.status(200).json(sortedProducts);
+app.get('/about', logger, (req, res) => {
+	return res.send('About');
 })
 
 app.listen(5000, () => {
