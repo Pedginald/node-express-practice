5a696c7f06030dd8a3dec5b71e7a7e30bd74a949
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-05 18:38:34 -0400

    Applying app.use for middleware to limit repeating

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const logger = require('./logger');
 
-app.get('/', logger, (req, res) => {
+app.use(logger);
+
+app.get('/', (req, res) => {
 	return res.send('Home');
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
 	return res.send('About');
 })
 
