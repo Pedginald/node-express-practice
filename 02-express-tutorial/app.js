f20cbc1f9d245348d7b1be25c20517ddd431b8ed
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-05 16:49:12 -0400

    Practicing using params and querys

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,41 +1,40 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data')
+const { products } = require('./data');
 
 app.get('/', (req, res) => {
-	res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+	return res.send('<h1>Home Page</h1><a href="/api/products">Products<a>')
 })
 
 app.get('/api/products', (req, res) => {
 	const newProducts = products.map(product => {
 		const { id, name, image } = product;
-		return {id, name, image};
+		return { id, name, image };
 	})
 	res.json(newProducts);
 })
 
 app.get('/api/products/:productID', (req, res) => {
-	 // console.log(req);
-	 // console.log(req.params);
+	console.log(req.params);
 	const { productID } = req.params;
 	const singleProduct = products.find(product => {
 		return product.id === Number(productID);
 	})
 	if (!singleProduct) {
-		return res.status(404).send('Product does not exist!')
+		return res.status(404).send('This product does not exist!');
 	}
 	return res.json(singleProduct);
 })
 
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
 	console.log(req.params);
-	return res.send('Hello World!')
+	return res.send('Hello World!');
 })
 
 app.get('/api/v1/query', (req, res) => {
-	// console.log(req.query);
+	let sortedProducts = [...products]
 	const { search, limit } = req.query;
-	let sortedProducts = [...products];
+	// console.log(req.query);
 
 	if (search) {
 		sortedProducts = sortedProducts.filter(product => {
@@ -45,10 +44,10 @@ app.get('/api/v1/query', (req, res) => {
 	if (limit) {
 		sortedProducts = sortedProducts.slice(0, Number(limit));
 	}
-	if (sortedProducts < 1) {
+	if (sortedProducts.length < 1) {
 		return res.status(200).json({ success: true, data: [] })
 	}
-	return res.status(200).json(sortedProducts);
+	res.status(200).json(sortedProducts);
 })
 
 app.listen(5000, () => {
