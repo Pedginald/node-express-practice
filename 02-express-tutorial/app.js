aa02c3211647fc991ce53fa1ff165e436327c9b9
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-09 11:01:37 -0400

    Creating a post request for a login page

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,20 @@
 const express = require('express');
-const authorize = require('./authorize');
 const app = express();
-const logger = require('./logger');
+const { people } = require('./data.js');
 
-app.use([logger, authorize]);
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('./methods-public'));
 
-app.get('/', (req, res) => {
-	return res.send('Home');
+app.get('/api/people', (req, res) => {
+	res.status(200).json({ success: true, data: people });
 })
 
-app.get('/about', (req, res) => {
-	return res.send('About');
-})
-
-app.get('/api/products', (req, res) => {
-	return res.send('Products');
-})
-
-app.get('/api/items', (req, res) => {
-	return res.send('Items')
+app.post('/login', (req, res) => {
+	const { name } = req.body;
+	if (name) {
+		return res.send(`Welcome ${name}`);
+	}
+	return res.send('Please provide a name')
 })
 
 app.listen(5000, () => {
