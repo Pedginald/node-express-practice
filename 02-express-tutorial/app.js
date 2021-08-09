837a8b7e7ba941972f38d476ccf786e25eb6c756
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-09 13:59:57 -0400

    Creating post request to update input names

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const { people } = require('./data.js');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static('./methods-public'));
+app.use(express.json());
 
 app.get('/api/people', (req, res) => {
-	res.status(200).json({ success: true, data: people });
+	return res.status(200).json({ success: true, data: people });
+})
+
+app.post('/api/people', (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res.status(400).json({ success: false, msg: 'Please provide name value'});
+	}
+	return res.status(201).json({ success: true, person: name});
 })
 
 app.post('/login', (req, res) => {
 	const { name } = req.body;
+	console.log(req.body);
 	if (name) {
 		return res.send(`Welcome ${name}`);
 	}
-	return res.send('Please provide a name')
+	return res.send('Please enter a name');
 })
 
 app.listen(5000, () => {
