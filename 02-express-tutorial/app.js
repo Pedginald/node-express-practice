adf0bf3a7e83b7f90a776edb3d7d1bbbc3284b5d
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-09 15:38:42 -0400

    Creating put and delete requests

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const { people } = require('./data.js');
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('./methods-public'));
 app.use(express.json());
+
+app.post('/login', (req, res) => {
+	const { name } = req.body;
+	console.log(req.body);
+	if (name) {
+		return res.send(`Welcome ${name}`);
+	}
+	return res.send('Please enter a name');
+})
 
 app.get('/api/people', (req, res) => {
 	return res.status(200).json({ success: true, data: people });
@@ -28,13 +37,36 @@ app.post('/api/postman/people', (req, res) => {
 	return res.status(201).json({ success: true, data: [...people, name] })
 })
 
-app.post('/login', (req, res) => {
+
+app.put('/api/people/:id', (req, res) => {
+	const { id } = req.params;
 	const { name } = req.body;
-	console.log(req.body);
-	if (name) {
-		return res.send(`Welcome ${name}`);
+
+	const person = people.find(person => {
+		return person.id === Number(id);
+	})
+
+	const newPeople = people.map(person => {
+		if (person.id === Number(id)) {
+			person.name = name;
+		}
+		return person;
+	})
+	return res.status(200).json({ success: true, data: newPeople });
+})
+
+app.delete('/api/people/:id', (req, res) => {
+	const { id } = req.params;
+	const person = people.find(person => {
+		return person.id === Number(id);
+	})
+	const newPeople = people.filter(person => {
+		return person.id !== Number(id);
+	})
+	if (!person) {
+		return res.status(400).json({ success: false, msg: `No person with the id ${id}`});
 	}
-	return res.send('Please enter a name');
+	return res.status(200).json({ success: true, data: newPeople});
 })
 
 app.listen(5000, () => {
